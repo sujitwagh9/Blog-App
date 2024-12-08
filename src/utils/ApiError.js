fff97f4c17b746581bcca7df0ee9013322bcee2c
Sujit Wagh <sujitwagh1233@gmail.com>
2024-12-09 03:06:49 +0530

    apiError added

---
+++ src/utils/ApiError.js
@@ -0,0 +1,13 @@
+// utils/ApiError.js
+
+class ApiError extends Error {
+    constructor(statusCode, message, isOperational = true) {
+      super(message);
+      this.statusCode = statusCode;
+      this.isOperational = isOperational;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  export default ApiError;
+  
