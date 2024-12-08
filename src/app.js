0890c8209d4bd331ef38f0949c46219d04941c63
Sujit Wagh <sujitwagh1233@gmail.com>
2024-12-09 03:07:51 +0530

    remaining changes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js"; 
+import articleRoutes from "./routes/article.routes.js";
+import errorHandler from "./middlewares/errorHandler.js"; 
 
-const app = express()
+const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    origin: process.env.CORS_ORIGIN, 
+    credentials: true,
+}));
 
 
-
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 
-export { app }
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+
+app.use("/api/users", userRoutes); 
+app.use("/api/articles", articleRoutes); 
+
+app.use(errorHandler);
+
+export { app };
