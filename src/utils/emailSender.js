7308fb90253135d30726748a446876a7ff4dbeef
Sujit Wagh <sujitwagh1233@gmail.com>
2025-02-06 16:49:46 +0530

    send email update

--- src/utils/emailSender.js
+++ src/utils/emailSender.js
@@ -19,5 +19,16 @@ const sendEmail= async (to,subject,text)=>{
 
 };
 
+const sendVerificationEmail=async(to,verificationCode)=>{
+    const subject="Verify Your Email Address";
+    const text=`Please verify your email address by entering the following code: ${verificationCode}`;
+    await sendEmail(to,subject,text);
+};
 
-export default sendEmail;
+const sendPasswordResetEmail=async(to,resetToken)=>{
+    const subject="Reset Your Password";
+    const text=`Please reset your password by clicking the following link: ${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
+    await sendEmail(to,subject,text);
+};
+
+export {sendEmail,sendVerificationEmail,sendPasswordResetEmail};
